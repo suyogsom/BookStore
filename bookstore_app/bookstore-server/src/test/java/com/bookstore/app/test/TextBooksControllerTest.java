32898e1b3e15a3b30b2af6f36dd9a41441014f23
suyogsom <courage.som@gmail.com>
2019-04-29 20:09:54 -0600

    integration test name changed

--- bookstore_app/bookstore-server/src/test/java/com/bookstore/app/test/TextBooksControllerTest.java
+++ bookstore_app/bookstore-server/src/test/java/com/bookstore/app/test/TextBooksControllerTest.java
@@ -6,6 +6,7 @@ import static org.mockito.Mockito.when;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.experimental.categories.Category;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
