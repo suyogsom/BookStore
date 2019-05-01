569334e4e9e5a69aba23e5c092a5dcc9b895b1b1
suyogsomavanshi <suyogsom>
2019-04-30 22:57:41 -0600

    lot of changes lets see

---
+++ bookstore_app/bookstore-integration-test/src/test/java/com/bookstore/it/TextBooksIntegrationTest.java
@@ -0,0 +1,21 @@
+package com.bookstore.it;
+
+import static io.restassured.RestAssured.given;
+
+import org.junit.jupiter.api.Test;
+
+import io.restassured.response.Response;
+
+public class TextBooksIntegrationTest extends BaseTest {
+	
+	@Test
+	public void testGetBooks() {
+		Response response = given().spec(restClient.getRecSpec())
+								   .basePath("/books")
+								   .get("/textbooks");
+		
+		//JsonPath getResponse = new JsonPath(response.asString());
+		//assertThat("1001",getResponse.get("[0].id"));
+		System.out.print(response.asString());
+	}
+}
