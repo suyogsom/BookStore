93420e4e0670183d2a9e2110d2ce5819d4e4fe2d
suyogsomavanshi <suyogsom>
2019-05-04 15:05:38 -0600

    chnages

--- bookstore_app/bookstore-integration-test/src/test/java/com/bookstore/it/TextBooksTestIntegration.java
+++ bookstore_app/bookstore-integration-test/src/test/java/com/bookstore/it/TextBooksTestIntegration.java
@@ -2,6 +2,8 @@ package com.bookstore.it;
 
 import static io.restassured.RestAssured.given;
 
+import java.util.Properties;
+
 import org.junit.jupiter.api.Test;
 
 import io.restassured.response.Response;
@@ -17,5 +19,14 @@ public class TextBooksTestIntegration extends BaseTest {
 		//JsonPath getResponse = new JsonPath(response.asString());
 		//assertThat("1001",getResponse.get("[0].id"));
 		System.out.print(response.asString());
+	}
+	
+	@Test
+	public void testRandom() {
+		System.out.println("Basedir : " + System.getProperty("user.dir"));
+		
+		Properties properties = System.getProperties();
+		properties.forEach((i,j) -> System.out.println("Property " + i + " value " + j));
+	
 	}
 }
