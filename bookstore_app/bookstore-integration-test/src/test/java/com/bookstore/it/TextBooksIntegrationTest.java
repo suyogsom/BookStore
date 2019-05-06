363f1dca334a4f8bcf6524ac2c6b6944b08126fc
Suyog Somavanshi <suyogsom>
2019-05-06 13:52:36 -0600

    final version

---
+++ bookstore_app/bookstore-integration-test/src/test/java/com/bookstore/it/TextBooksIntegrationTest.java
@@ -0,0 +1,37 @@
+package com.bookstore.it;
+
+import static io.restassured.RestAssured.given;
+
+import org.junit.jupiter.api.Test;
+
+import io.restassured.response.Response;
+
+public class TextBooksIntegrationTest extends BaseTest {
+	
+	@Test
+	public void testGetBooks() {
+		Response response = given().spec(restClient.getRecSpec())
+								   .basePath("/books")
+								   .get("/textbooks");
+		
+		//JsonPath getResponse = new JsonPath(response.asString());
+		//assertThat("1001",getResponse.get("[0].id"));
+		System.out.print(response.asString());
+	}
+	
+	@Test
+	public void testRandom() {
+		System.out.println("Basedir : " + System.getProperty("user.dir"));
+		
+		//Properties properties = System.getProperties();
+		//properties.forEach((i,j) -> System.out.println("Property " + i + " value " + j));
+		
+		//System.out.println("user home : " + System.getProperty("user.home"));
+	
+	}
+	
+	@Test
+	public void hello() {
+		System.out.print("Hello");
+	}
+}
--- bookstore_app/bookstore-server/src/main/java/com/bookstore/app/repositories/TextBooksRepository.java
+++ bookstore_app/bookstore-server/src/main/java/com/bookstore/app/repositories/TextBooksRepository.java
@@ -6,6 +6,21 @@ import com.bookstore.app.models.TextBooks;
 
 public interface TextBooksRepository extends CrudRepository<TextBooks, String>{ 
 	
+//	private List<TextBooks> datastore;
+//	
+//	//loading the data
+//	@PostConstruct() {
+//		//read json
+//		//map the json to TextBook model using jackson
+//		//load json data to text book objects
+//		//store the objects in datastore
+//	}
+//	
+//	public TextBook getTextBook(String id) {
+//		datastore.
+//	}
+	
+	
 }
 
 
