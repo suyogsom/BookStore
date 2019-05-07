e2c500342bd3b7e1fa8a1715c1131ff4789faac6
Suyog Somavanshi <suyogsom>
2019-05-06 21:45:48 -0600

    finale

--- bookstore_app/bookstore-server/src/test/java/com/bookstore/app/test/TextBooksControllerTest.java
+++ bookstore_app/bookstore-server/src/test/java/com/bookstore/app/test/TextBooksControllerTest.java
@@ -20,7 +20,7 @@ import com.bookstore.app.services.TextBooksService;
 
 
 @RunWith(MockitoJUnitRunner.class)
-public class TextBooksControllerUnitTest {
+public class TextBooksControllerTest {
 	
     @InjectMocks
     TextBooksController textBooksController;
